3a7df88de0c9dcbc5fdcf8a55278f550b43f0283
Edvards Mazprecnieks <edvards.mazprecnieks@code.berlin>
2023-03-23 14:59:13 +0100

    Rename variables

--- markdown.py
+++ markdown.py
@@ -15,29 +15,29 @@ def parse(markdown):
     """
 
     lines = markdown.split("\n")
-    res = ""
+    html = ""
     in_list = False
     in_list_append = False
-    for i in lines:
-        if i.startswith("###### "):
-            i = "<h6>" + i[7:] + "</h6>"
-        elif i.startswith("##### "):
-            i = "<h5>" + i[6:] + "</h5>"
-        elif i.startswith("#### "):
-            i = "<h4>" + i[5:] + "</h4>"
-        elif i.startswith("### "):
-            i = "<h3>" + i[4:] + "</h3>"
-        elif i.startswith("## "):
-            i = "<h2>" + i[3:] + "</h2>"
-        elif i.startswith("# "):
-            i = "<h1>" + i[2:] + "</h1>"
+    for line in lines:
+        if line.startswith("###### "):
+            line = "<h6>" + line[7:] + "</h6>"
+        elif line.startswith("##### "):
+            line = "<h5>" + line[6:] + "</h5>"
+        elif line.startswith("#### "):
+            line = "<h4>" + line[5:] + "</h4>"
+        elif line.startswith("### "):
+            line = "<h3>" + line[4:] + "</h3>"
+        elif line.startswith("## "):
+            line = "<h2>" + line[3:] + "</h2>"
+        elif line.startswith("# "):
+            line = "<h1>" + line[2:] + "</h1>"
 
-        elif i.startswith("* "):
+        elif line.startswith("* "):
             if not in_list:
                 in_list = True
                 is_bold = False
                 is_italic = False
-                curr = i[2:]
+                curr = line[2:]
                 if "__" in curr:
                     parts = curr.split("__")
                     curr = parts[0] + "<strong>" + parts[1] + "</strong>" + parts[2]
@@ -46,11 +46,11 @@ def parse(markdown):
                     parts = curr.split("_")
                     curr = parts[0] + "<em>" + parts[1] + "</em>" + parts[2]
                     is_italic = True
-                i = "<ul><li>" + curr + "</li>"
+                line = "<ul><li>" + curr + "</li>"
             else:
                 is_bold = False
                 is_italic = False
-                curr = i[2:]
+                curr = line[2:]
                 if "__" in curr:
                     is_bold = True
                 if "_" in curr:
@@ -61,28 +61,28 @@ def parse(markdown):
                 if is_italic:
                     parts = curr.split("_")
                     curr = parts[0] + "<em>" + parts[1] + "</em>" + parts[2]
-                i = "<li>" + curr + "</li>"
+                line = "<li>" + curr + "</li>"
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
         if not (
-            i.startswith("<h")
-            or i.startswith("<ul")
-            or i.startswith("<p")
-            or i.startswith("<li")
+            line.startswith("<h")
+            or line.startswith("<ul")
+            or line.startswith("<p")
+            or line.startswith("<li")
         ):
-            i = "<p>" + i + "</p>"
-        if "__" in i:
-            parts = i.split("__")
-            i = parts[0] + "<strong>" + parts[1] + "</strong>" + parts[2]
-        if "_" in i:
-            parts = i.split("_")
-            i = parts[0] + "<em>" + parts[1] + "</em>" + parts[2]
+            line = "<p>" + line + "</p>"
+        if "__" in line:
+            parts = line.split("__")
+            line = parts[0] + "<strong>" + parts[1] + "</strong>" + parts[2]
+        if "_" in line:
+            parts = line.split("_")
+            line = parts[0] + "<em>" + parts[1] + "</em>" + parts[2]
         if in_list_append:
-            i = "</ul>" + i
+            line = "</ul>" + line
             in_list_append = False
-        res += i
+        html += line
     if in_list:
-        res += "</ul>"
-    return res
+        html += "</ul>"
+    return html
